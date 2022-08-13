a95d8225a45e008ba9f7f0d1e6c01ed7d77bca9d
owen-hwlee <55585505+owen-hwlee@users.noreply.github.com>
2022-08-14 03:54:12 +0800

    Time: 3 ms (59.20%), Space: 5.9 MB (79.07%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        if (n == 0) return 0;
-        short count = 0;
-        do {
-            count += n & 1;
-            n = n >> 1;
-        } while (n > 0);
-        return count;
+        return __builtin_popcount(n);
     }
 };
