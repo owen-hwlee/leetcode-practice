f477b23352f6c8d5616a9e25dcd7a7740e5a4b34
owen-hwlee <55585505+owen-hwlee@users.noreply.github.com>
2023-09-27 03:00:38 +0800

    Time: 11 ms (84.79%), Space: 41.3 MB (79.22%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.java
@@ -0,0 +1,47 @@
+class Foo {
+
+    private int printCount = 1;
+    
+    public Foo() {
+        
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        synchronized (this) {
+            while (printCount != 1) {
+                wait();
+            }
+            
+            // printFirst.run() outputs "first". Do not change or remove this line.
+            printFirst.run();
+            ++printCount;
+            notifyAll();
+        }
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        synchronized (this) {
+            while (printCount != 2) {
+                wait();
+            }
+            
+            // printSecond.run() outputs "second". Do not change or remove this line.
+            printSecond.run();
+            ++printCount;
+            notifyAll();
+        }
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        synchronized (this) {
+            while (printCount != 3) {
+                wait();
+            }
+            
+            // printThird.run() outputs "third". Do not change or remove this line.
+            printThird.run();
+            ++printCount;
+            notifyAll();
+        }
+    }
+}
