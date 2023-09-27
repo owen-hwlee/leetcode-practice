027fdaf54555b5e2e8d1b5a2dda2857d7e89634f
owen-hwlee <55585505+owen-hwlee@users.noreply.github.com>
2023-09-27 16:53:50 +0800

    Time: 51 ms (75.75%), Space: 42.2 MB (94.38%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.ts
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.ts
@@ -1,7 +1,12 @@
 function map(arr: number[], fn: (n: number, i: number) => number): number[] {
-	const returnedArray: number[] = [];
+	// const returnedArray: number[] = [];
+	// for (let i: number = 0; i < arr.length; ++i) {
+	// returnedArray.push(fn(arr[i], i));
+	// }
+	// return returnedArray;
+    
     for (let i: number = 0; i < arr.length; ++i) {
-        returnedArray.push(fn(arr[i], i));
+        arr[i] = fn(arr[i], i);
     }
-    return returnedArray;
+    return arr;
 };
