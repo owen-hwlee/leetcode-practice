4cd88db6da09bdc2e34201d56122dd6a69715a0e
owen-hwlee <55585505+owen-hwlee@users.noreply.github.com>
2023-10-01 05:25:06 +0800

    Time: 1700 ms (77.11%), Space: 0B (100%) - LeetHub

--- 1541-top-travellers/1541-top-travellers.sql
+++ 1541-top-travellers/1541-top-travellers.sql
@@ -3,5 +3,5 @@ SELECT name, IFNULL(SUM(distance), 0) AS travelled_distance
 FROM Users
 LEFT JOIN Rides
 ON Users.id = Rides.user_id
-GROUP BY Rides.user_id
+GROUP BY Users.id
 ORDER BY travelled_distance DESC, name ASC;
