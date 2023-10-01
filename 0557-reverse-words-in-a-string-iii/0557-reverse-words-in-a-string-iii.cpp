2c1f38d1572038583d81c117b329a0bba3bd7509
owen-hwlee <55585505+owen-hwlee@users.noreply.github.com>
2023-10-01 20:25:10 +0800

    Time: 16 ms (55.57%), Space: 10 MB (59.58%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
     string reverseWords(string s) {
-        size_t i = 0, j = 0;   // (i -> start, j -> end) of each word
+        size_t start = 0;   // start index of each word
         const size_t& size = s.size();
 
-        for (size_t k = 1; k < size; ++k) {
-            if (s[k] == ' ') {
-                // whitespace reached, start swapping
-                swap_char(s, i, j);
-                // update i, j to start index of next word
-                i = j = k + 1;
-            } else {
-                // other ascii char reached, update j
-                j = k;
+        for (size_t i = 1; i < size; ++i) {
+            if (s[i] == ' ' || i == size - 1) {
+                // whitespace or end of string reached, start swapping
+                size_t end = (i != size - 1) ? i - 1 : i;
+                size_t num_swap = (end + 1 - start) / 2;
+                for (size_t j = 0; j < num_swap; ++j) {
+                    swap(s[start + j], s[end - j]);
+                }
+
+                start = i + 1;  // update start to index of next word
             }
         }
-        // no trailing spaces, manually swap last word
-        swap_char(s, i, j);
 
         return s;
-    }
-private:
-    inline void swap_char(string& s, const size_t& i, const size_t& j) {
-        size_t num_swap = (j + 1 - i) / 2;
-        for (size_t k = 0; k < num_swap; ++k) {
-            swap(s[i + k], s[j - k]);
-        }
     }
 };
