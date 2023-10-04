db1f0bddef53f1c374f72b0341fc7871f8b9bf2d
owen-hwlee <55585505+owen-hwlee@users.noreply.github.com>
2023-10-04 22:50:05 +0800

    Time: 22 ms (86.21%), Space: 11.5 MB (6.9%) - LeetHub

---
+++ 0817-design-hashmap/0817-design-hashmap.rs
@@ -0,0 +1,37 @@
+struct MyHashMap {
+    values: Vec<i32>,
+}
+
+
+/** 
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl MyHashMap {
+
+    fn new() -> Self {
+        MyHashMap {
+            values: vec![-1; 1000001],
+        }
+    }
+    
+    fn put(&mut self, key: i32, value: i32) {
+        self.values[key as usize] = value;
+    }
+    
+    fn get(&self, key: i32) -> i32 {
+        self.values[key as usize]
+    }
+    
+    fn remove(&mut self, key: i32) {
+        self.values[key as usize] = -1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * let obj = MyHashMap::new();
+ * obj.put(key, value);
+ * let ret_2: i32 = obj.get(key);
+ * obj.remove(key);
+ */
