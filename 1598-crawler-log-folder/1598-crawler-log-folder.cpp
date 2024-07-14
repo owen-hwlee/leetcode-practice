d210663c937d8083d88d7aa479315ea507360df4
owen-hwlee <55585505+owen-hwlee@users.noreply.github.com>
2024-07-14 00:37:31 -0700

    Time: 4 ms (68.52%), Space: 14.3 MB (16.8%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.cpp
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     int minOperations(vector<string>& logs) {
         int count = 0;
-        regex re("^[a-z0-9]+\/$");
         for (string log: logs) {
-            if (log.compare("../") == 0 && count > 0) {     // go to parent
-                --count;
-            } else if (regex_match(log, re)) {              // go to child
+            if (log == "../") {         // go to parent
+                if (count > 0) {
+                    --count;
+                }
+            } else if (log != "./") {   // go to child
                 ++count;
             }
         }
